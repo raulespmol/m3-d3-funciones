8d7311b67ae320696ec4cfd9b632f69aa55138dd
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-28 20:50:56 -0300

    pintar

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const ele = document.getElementById("ele1")
+
+ele.addEventListener("click", function(){
+  pintar('yellow')
+});
+
+function pintar(color='green'){
+  ele.style.backgroundColor = color
+}
