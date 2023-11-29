ab3582047fe67be5933175f74815fcf1e099ba5b
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-28 21:28:14 -0300

    carpeta requerimientos 1 y 2

--- 1-2/1_funcion.js
+++ 1-2/1_funcion.js
@@ -1,6 +1,7 @@
 /*------------------------------------------------------------
 
-  1. Transforma esta declaración de función a una función de expresión. El ejercicio debe quedar en un archivo nombrado `1_funcion.js` (1 Punto)
+  1. Transforma esta declaración de función a una función de expresión. 
+  El ejercicio debe quedar en un archivo nombrado `1_funcion.js` (1 Punto)
   
     function example(a, b, c){
       return a+b+c
--- 1-2/2_arrow.js
+++ 1-2/2_arrow.js
@@ -1,6 +1,7 @@
 /*------------------------------------------------------------
 
-  2. Transforma la siguiente función a una arrow function de una línea. Este ejercicio debe quedar en un archivo llamado `2_arrow.js` (1 Punto)
+  2. Transforma la siguiente función a una arrow function de una línea. 
+  Este ejercicio debe quedar en un archivo llamado `2_arrow.js` (1 Punto)
 
     let suma = function(a, b){
       return a + b
