d216ed54565e93e1db8747d93e4283c220f6e331
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-30 19:35:35 -0300

    4 colores, segundo metodo
    script más largo, selecciona cada elemento y requiere más de un event listener

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,6 +1,16 @@
-document.addEventListener('click', e => {
-  const element = e.target;
-  if(element.className === 'box'){
-    element.style.backgroundColor = '#000';
-  }
-})
+const A = document.getElementById('A');
+const B = document.getElementById('B');
+const C = document.getElementById('C');
+const D = document.getElementById('D');
+
+A.addEventListener('click', e => {paintBlack(e.target)})
+
+B.addEventListener('click', e => {paintBlack(e.target)})
+
+C.addEventListener('click', e => {paintBlack(e.target)})
+
+D.addEventListener('click', e => {paintBlack(e.target)})
+
+function paintBlack(element){
+  element.style.backgroundColor = '#000'
+}
