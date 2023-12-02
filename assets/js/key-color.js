d50689e579398dd178caa2fe0caa97c9f2e87f08
Raul Espinoza M <raul.espmol@gmail.com>
2023-12-02 11:31:07 -0300

    crear div una sola vez con QWE

--- assets/js/key-color.js
+++ assets/js/key-color.js
@@ -1,4 +1,7 @@
+const container = document.querySelector('main')
 const keyDiv = document.getElementById('key');
+
+//Teclas ASD
 let color = '#FFF';
 
 document.addEventListener('keydown', e => {
@@ -11,6 +14,33 @@ document.addEventListener('keydown', e => {
   }
   keyDiv.style.backgroundColor = color
 })
+
+//Teclas QWE
+//Crear div
+const keyDiv2 = document.createElement('div')
+keyDiv2.id = 'key2';
+let keys = ['q', 'w', 'e', 'Q', 'W', 'E']
+
+document.addEventListener('keydown', e => {
+  if(keys.includes(e.key) && container.childElementCount == 1){
+    container.appendChild(keyDiv2);
+  }
+})
+
+//Cambiar color
+let color2 = '#FFF';
+
+document.addEventListener('keydown', e => {
+  if(e.key === 'q' || e.key === 'Q'){
+    color2 = '#909'
+  } else if (e.key === 'w' || e.key === 'W'){
+    color2 = '#888'
+  } else if (e.key === 'e' || e.key === 'E'){
+    color2 = '#530'
+  }
+  keyDiv2.style.backgroundColor = color2
+})
+
 
 
 
